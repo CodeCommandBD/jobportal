e208ce40bb313e7749f6850ea0ff5a52cf1469d9
shanto <shantokumar22@gmail.com>
2026-03-26 00:33:23 +0600

    chore: convert public index to JS

---
+++ public/index.js
@@ -0,0 +1,14 @@
+const images = {
+  herobg: '/Assets/hero.png',
+  jobone: '/Assets/j1.png',
+  jobtwo: '/Assets/j2.png',
+  jobthree: '/Assets/j3.png',
+  jobfour: '/Assets/j4.png',
+  jobfive: '/Assets/j5.png',
+  jobsix: '/Assets/j6.png',
+  jobseven: '/Assets/j7.png',
+  jobeight: '/Assets/j8.png',
+  jobnine: '/Assets/j9.png'
+}
+
+export default images 
