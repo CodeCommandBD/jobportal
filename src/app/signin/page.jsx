04d1255fb2af70cfc5d6b73c74ed3f8ed29ba971
shanto <shantokumar22@gmail.com>
2026-03-27 03:18:04 +0600

    chore: convert signin page to JSX

---
+++ src/app/signin/page.jsx
@@ -0,0 +1,12 @@
+import LoginForm from '@/Components/auth/LoginForm'
+import React from 'react'
+
+const SignInPage = () => {
+  return (
+    <div className='min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900 px-4'>
+        <LoginForm />
+    </div>
+  )
+}
+
+export default SignInPage
