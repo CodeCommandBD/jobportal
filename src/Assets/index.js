fb1937a56de385df6b33adabfed7f2376277a1ca
shanto <shantokumar22@gmail.com>
2026-03-27 03:20:27 +0600

    chore: convert assets index to JS

---
+++ src/Assets/index.js
@@ -0,0 +1,16 @@
+import herobg from './images/hero.png'
+import jobcardone from './images/j1.png'
+import jobcardtwo from './images/j2.png'
+import jobcardthree from './images/j3.png'
+import jobcardfour from './images/j4.png'
+import jobcardfive from './images/j5.png'
+import jobcardsix from './images/j6.png'
+import jobcardseven from './images/j7.png'
+import jobcardeight from './images/j8.png'
+import jobcardnine from './images/j9.png'
+import infoimg from './images/a.png'
+import reviewone from './images/u1.png'
+import reviewtwo from './images/u2.png'
+import reviewthree from './images/u3.png'
+
+export {reviewone,reviewtwo,reviewthree ,infoimg,herobg, jobcardone, jobcardtwo, jobcardthree, jobcardfour, jobcardfive, jobcardsix, jobcardseven, jobcardeight, jobcardnine}
