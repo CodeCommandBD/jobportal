f60fd7aa51e1ac483cfa29b8a4f40414be56a206
shanto <shantokumar22@gmail.com>
2026-03-27 03:15:39 +0600

    chore: convert findjob page to JSX

---
+++ src/app/findjob/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import FindJobContent from './FindJobContent'
+
+export const metadata = {
+  title: 'Find Your Dream Job | JobPortal',
+  description: 'Search and apply for the latest job openings in software development, design, marketing, and more.',
+}
+
+const FindJobPage = () => {
+  return <FindJobContent />
+}
+
+export default FindJobPage
