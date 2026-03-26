fe290104672ceb3888086204836903686cd2df3e
shanto <shantokumar22@gmail.com>
2026-03-27 03:00:10 +0600

    chore: convert contact page to JSX

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ContactContent from './ContactContent'
+
+export const metadata = {
+  title: 'Contact Us | JobPortal',
+  description: 'Have questions? Get in touch with our team for support or inquiries.',
+}
+
+const ContactPage = () => {
+  return <ContactContent />
+}
+
+export default ContactPage
