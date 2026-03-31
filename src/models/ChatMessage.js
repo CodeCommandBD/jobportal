92d12a2e5a2b4a2e3ec606fe161bfac45dc0cb22
shanto <shantokumar22@gmail.com>
2026-03-31 22:48:28 +0600

    chore: convert ChatMessage model to JS

---
+++ src/models/ChatMessage.js
@@ -0,0 +1,25 @@
+
+import mongoose from 'mongoose';
+
+const ChatMessageSchema = new mongoose.Schema({
+  conversationId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Conversation',
+    required: true
+  },
+  senderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  isRead: {
+    type: Boolean,
+    default: false
+  }
+}, { timestamps: true });
+
+export default mongoose.models.ChatMessage || mongoose.model('ChatMessage', ChatMessageSchema);
