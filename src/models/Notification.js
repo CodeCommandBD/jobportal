076a64b67b17f8675ca2c43061e1538cdf96e79c
shanto <shantokumar22@gmail.com>
2026-04-09 09:34:13 +0600

    Add Notification model

---
+++ src/models/Notification.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const NotificationSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ['info', 'success', 'warning', 'error'],
+    default: 'info',
+  },
+  isRead: {
+    type: Boolean,
+    default: false,
+  },
+  link: {
+    type: String, // Link to where to go when the notification is clicked
+  }
+}, { timestamps: true });
+
+export default mongoose.models.Notification || mongoose.model('Notification', NotificationSchema);
