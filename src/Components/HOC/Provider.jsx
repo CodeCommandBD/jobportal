a9049976f072e12443111d578c518c9938f4ebee
shanto <shantokumar22@gmail.com>
2026-04-10 10:44:18 +0600

    refactor: update QueryClient provider configuration

--- src/Components/HOC/Provider.jsx
+++ src/Components/HOC/Provider.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Provider = ({children}) => {
   return (
-    <ThemeProvider attribute='class'  defaultTheme='dark' enableSystem>
+    <ThemeProvider attribute='class' defaultTheme='light' enableSystem>
         {children}
     </ThemeProvider>
   )
